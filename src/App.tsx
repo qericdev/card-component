c9879ca33e7cd2955e993c8b14eee84b6e7143f0
qericdev <101020988+qericdev@users.noreply.github.com>
2022-08-24 19:03:06 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() : JSX.Element{
+  return (
+    <div className='App align-v-h'>
+      <div className='product align-v-h '>
+        <img className="img-desktop"alt="parfum" src="./../images/image-product-desktop.jpg" />
+        <img className="img-mobile" alt="parfum" src="./../images/image-product-mobile.jpg" />
+        <section className='details'>
+          <h2>PERFUME</h2>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <div className='price align-v-h'>
+            <p>$149.99</p>
+            <p>$169.99</p>
+          </div>
+          <button className='align-v-h'>
+            <img alt='Shopping Cart' src='./../images/icon-cart.svg'/>
+            <p>Add to Cart</p>
+          </button>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
